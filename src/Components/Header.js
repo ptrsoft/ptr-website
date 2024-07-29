4b56d2eec2278952883b7f5f06488d5f129c15d3
furqhan bin moosa <furqhanbinmoosaptr@gmail.com>
2024-07-29 11:06:17 +0530

    mobile menu added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,69 +1,122 @@
-import React, { useEffect, useState } from 'react'
-import '../Assets/Styles/Components/Header.css'
-import { Link , useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import '../Assets/Styles/Components/Header.css';
+import { Link, useLocation } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useMediaQuery } from '@mui/material';
 
 const Header = () => {
-    const location = useLocation();
-    const [scrolled, setScrolled] = useState(false);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 50) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    return (
-      <header className={`header space-container ${scrolled ? 'scrolled' : ''}`}>
-        <div className='header_left'>
+  const [state, setState] = React.useState({
+    open: false,
+  });
+
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem>
+          <ListItemButton component={Link} to="/">
+            <ListItemText  className='mobile_links' primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component={Link} to="/about-us">
+            <ListItemText  className='mobile_links' primary="About Us" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component={Link} to="/process">
+            <ListItemText  className='mobile_links' primary="Process" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component={Link} to="/contact-us">
+            <ListItemText  className='mobile_links' primary="Contact Us" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  const location = useLocation();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`header space-container ${scrolled ? 'scrolled' : ''}`}>
+      <div className='header_left'>
         <Link to="/">
           <img className='header_logo' alt='Logo' src='/Images/PTRLogo.png' />
         </Link>
 
-          <ul className='flex gap-[3.56rem]'>
-            <li className={location.pathname === '/' ? 'active' : ''}>
-              <Link to="/">Home</Link>
-            </li>
-            <li className={location.pathname === '/about-us' ? 'active' : ''}>
-              <Link to="/about-us">About Us</Link>
-            </li>
-
-            <li className={location.pathname === '/process' ? 'active' : ''}>
-              <Link to="/process">Process</Link>
-            </li>
-
-            <li className={location.pathname === '/contact-us' ? 'active' : ''}>
-              <Link to="/contact-us">Contact</Link>
-            </li>
-
-
-            {/* <li className={location.pathname === '/products' ? 'active' : ''}>
-              <Link to="/not-found">Products</Link>
-            </li> */}
-            {/* <li className={location.pathname === '/process' ? 'active' : ''}>
-              <Link to="/process">Process</Link>
-            </li> */}
-            {/* <li>
-              <Link to="/not-found">Blogs</Link>
-            </li> */}    {/* <li>
-              <Link to="/not-found">Blogs</Link>
-            </li> */}
-          </ul>
-        </div>
+        <ul className='flex gap-[3.56rem]'>
+          <li className={location.pathname === '/' ? 'active' : ''}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={location.pathname === '/about-us' ? 'active' : ''}>
+            <Link to="/about-us">About Us</Link>
+          </li>
+          <li className={location.pathname === '/process' ? 'active' : ''}>
+            <Link to="/process">Process</Link>
+          </li>
+          <li className={location.pathname === '/contact-us' ? 'active' : ''}>
+            <Link to="/contact-us">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='header_right_box'>
         <button>
           <Link to="/contact-us">Get In Touch</Link>
         </button>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+        <span className='responsive_menu' edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+          <MenuIcon style={{fontSize:27}} />
+        </span>
+        <Drawer
+          anchor={isSmallScreen ? 'top' : 'right'}
+          open={state.open}
+          onClose={toggleDrawer(false)}
+        >
+          {list(isSmallScreen ? 'top' : 'right')}
+        </Drawer>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
